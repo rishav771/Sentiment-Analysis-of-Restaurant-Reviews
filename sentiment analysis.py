f7a17e86a7d763d7bb00d49e719d6a76e2c53520
Rishavraj Prasad <52820564+rishav771@users.noreply.github.com>
2021-05-04 21:45:02 +0530

    Add files via upload

---
+++ sentiment analysis.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('../input/Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+    
+# Creating the Bag of Words model using CountVectorizer
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+
+
+# Multinomial NB
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB(alpha=0.1)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print ("Confusion Matrix:\n",cm)
+
+# Accuracy, Precision and Recall
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+score1 = accuracy_score(y_test,y_pred)
+score2 = precision_score(y_test,y_pred)
+score3= recall_score(y_test,y_pred)
+print("\n")
+print("Accuracy is ",round(score1*100,2),"%")
+print("Precision is ",round(score2,2))
+print("Recall is ",round(score3,2))
+
+
+
+
+# Bernoulli NB
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import BernoulliNB
+classifier = BernoulliNB(alpha=0.8)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print ("Confusion Matrix:\n",cm)
+
+# Accuracy, Precision and Recall
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+score1 = accuracy_score(y_test,y_pred)
+score2 = precision_score(y_test,y_pred)
+score3= recall_score(y_test,y_pred)
+print("\n")
+print("Accuracy is ",round(score1*100,2),"%")
+print("Precision is ",round(score2,2))
+print("Recall is ",round(score3,2))
+
+
+
+# Logistic Regression
+
+# Fitting Logistic Regression to the Training set
+from sklearn import linear_model
+classifier = linear_model.LogisticRegression(C=1.5)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print ("Confusion Matrix:\n",cm)
+
+# Accuracy, Precision and Recall
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+score1 = accuracy_score(y_test,y_pred)
+score2 = precision_score(y_test,y_pred)
+score3= recall_score(y_test,y_pred)
+print("\n")
+print("Accuracy is ",round(score1*100,2),"%")
+print("Precision is ",round(score2,2))
+print("Recall is ",round(score3,2))
